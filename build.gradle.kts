748cdecbb7dbe6e2aecef0ab93dc409231a87781
비엔나 <sure@paran.com>
2023-02-10 13:41:22 +0900

    add : jwt test code

--- build.gradle.kts
+++ build.gradle.kts
@@ -16,7 +16,18 @@ dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-web")
 	implementation("org.mybatis.spring.boot:mybatis-spring-boot-starter:3.0.0")
 	implementation("javax.xml.bind:jaxb-api:2.3.0")
-	implementation("io.jsonwebtoken:jjwt:0.9.1")
+	
+	// lombok
+	compileOnly("org.projectlombok:lombok:1.18.26")
+	annotationProcessor("org.projectlombok:lombok:1.18.26")
+	
+	// security
+	implementation("org.springframework.boot:spring-boot-starter-security")
+	
+	// jwt
+	implementation("io.jsonwebtoken:jjwt-api:0.11.5")
+	runtimeOnly("io.jsonwebtoken:jjwt-impl:0.11.5")
+	runtimeOnly("io.jsonwebtoken:jjwt-jackson:0.11.5")
 	runtimeOnly("com.mysql:mysql-connector-j")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 }
--- src/main/java/com/jwt/web/interceptor/interceptor.java
+++ src/main/java/com/jwt/web/interceptor/interceptor.java
@@ -10,22 +10,22 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
 @Component
-public class interceptor implements HandlerInterceptor{
+public class interceptor implements HandlerInterceptor {
 	@Override
 	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
 			throws Exception {
-		System.out.println("getHeader= "+request.getHeader("Authorization"));
+		System.out.println("getHeader= " + request.getHeader("Authorization"));
 		// 헤더에서 가져온 토큰 유효성 검사
 		String token = request.getHeader("Authorization");
-		if(token != null) {
+		if (token != null) {
 			System.out.println("토큰 보유");
 			return jwtProvider.validateToken(token);
-		}
-		else {
+		} else {
 			System.out.println("토큰 없어");
 			return false;
 		}
 	}
+
 	@Override
 	public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler,
 			ModelAndView modelAndView) throws Exception {
